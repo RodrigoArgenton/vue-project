4a7556c9c94537e74639c6dae5e3ea95e99d96f8
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-06-12 19:56:42 -0300

    Exemplo básico de interpolação

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 <script setup>
   // import ExemploBasico01 from './componentes/ExemploConcatenacao01.vue'
   // import ExemploBasico02 from './componentes/Exemplo02Condicional.vue'
-  import ExemploBasico03 from './componentes/Exemplo03For.vue';
+  // import ExemploBasico03 from './componentes/Exemplo03For.vue'
+  import ExemploBasico04 from './componentes/Exemplo04Interpolacao.vue'
 </script>
 
 
 <!-- HTML -->
 <template>
-  <ExemploBasico03></ExemploBasico03>
+  <ExemploBasico04></ExemploBasico04>
 </template>
