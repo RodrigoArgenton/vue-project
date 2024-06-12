18fc60211395d40a69a1a8bba5a498a597df3ef4
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-06-12 19:43:47 -0300

    Exemplo de utilização do For no Vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <!-- Script -->
 <script setup>
   // import ExemploBasico01 from './componentes/ExemploConcatenacao01.vue'
-  import ExemploBasico02 from './componentes/ExemploCondicional02.vue'
+  // import ExemploBasico02 from './componentes/Exemplo02Condicional.vue'
+  import ExemploBasico03 from './componentes/Exemplo03For.vue';
 </script>
 
 
 <!-- HTML -->
 <template>
-  <ExemploBasico02></ExemploBasico02>
+  <ExemploBasico03></ExemploBasico03>
 </template>
---
+++ src/componentes/Exemplo03For.vue
@@ -0,0 +1,11 @@
+<!-- Script -->
+<script setup>
+    let nomes = ['Alice', 'Bruna', 'Caio', 'Daniel', 'Elise', 'Fabio', 'Gabriel', 'Higor']
+</script>
+
+<!-- HTML -->
+ <template>
+    <ul>
+        <li v-for="nome in nomes">{{ nome }}</li>
+    </ul>
+ </template>
