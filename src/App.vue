ba5b21d3fd9dce3af42e1f89a6c68ff0eb08af33
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-06-12 15:12:19 -0300

    exemplo básico de concatenação de texto e números

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<!-- Script -->
+<script setup>
+  import ExemploBasico01 from './componentes/ExemploBasico01.vue'
+</script>
+
+
+<!-- HTML -->
+<template>
+  <ExemploBasico01></ExemploBasico01>
+</template>
---
+++ src/componentes/ExemploBasico01.vue
@@ -0,0 +1,11 @@
+<script setup>
+  let nome = 'Alice'
+  let idade = 38
+  let numero1 = 5
+  let numero2 = 10
+</script>
+
+<template>
+  <p>{{ 'Olá ' + nome + ' sua idade é ' + idade}}</p>
+  <p>{{ numero1 + numero2 }}</p>
+</template>
