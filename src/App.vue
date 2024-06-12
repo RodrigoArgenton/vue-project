cb8d3337b919aaaf8996bde23351b2f348efe7fd
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-06-12 15:38:26 -0300

    Exemplo básico utilizando o v-if em uma tag h1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <!-- Script -->
 <script setup>
-  import ExemploBasico01 from './componentes/ExemploBasico01.vue'
+  // import ExemploBasico01 from './componentes/ExemploConcatenacao01.vue'
+  import ExemploBasico02 from './componentes/ExemploCondicional02.vue'
 </script>
 
 
 <!-- HTML -->
 <template>
-  <ExemploBasico01></ExemploBasico01>
+  <ExemploBasico02></ExemploBasico02>
 </template>
---
+++ src/componentes/ExemploCondicional02.vue
@@ -0,0 +1,10 @@
+<!-- Script -->
+<script setup>
+    let exibir = true
+</script>
+
+
+<!-- HTML -->
+<template>
+    <h1 v-if="exibir">Hello World</h1>
+</template>
