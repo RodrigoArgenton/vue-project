a48c269500ee28e331a01d636ac94434f762cdd6
Rodrigo Argenton <rodrigolk4321@gmail.com>
2024-06-12 20:01:44 -0300

    Utilização do v-model e ref em um input simples

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,12 @@
   // import ExemploBasico01 from './componentes/ExemploConcatenacao01.vue'
   // import ExemploBasico02 from './componentes/Exemplo02Condicional.vue'
   // import ExemploBasico03 from './componentes/Exemplo03For.vue'
-  import ExemploBasico04 from './componentes/Exemplo04Interpolacao.vue'
+  // import ExemploBasico04 from './componentes/Exemplo04Interpolacao.vue'
+  import ExemploBasico05 from './componentes/Exemplo05Model.vue'
 </script>
 
 
 <!-- HTML -->
 <template>
-  <ExemploBasico04></ExemploBasico04>
+  <ExemploBasico05></ExemploBasico05>
 </template>
---
+++ src/componentes/Exemplo05Model.vue
@@ -0,0 +1,15 @@
+<!-- Script -->
+<script setup>
+import { ref } from 'vue';
+
+
+    let texto = ref('')
+
+</script>
+
+
+<!-- HTML -->
+<template>
+    <input type="text" v-model="texto">
+    <p>{{ texto }}</p>
+</template>
